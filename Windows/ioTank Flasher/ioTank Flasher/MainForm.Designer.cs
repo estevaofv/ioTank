bb893be80677fb5d998fbe58ad51d0fa12d6d82b
John Spounias <d3cline@pyl0n.com>
2017-06-07 18:22:38 -0600

    windows flasher update

--- Windows/ioTank Flasher/ioTank Flasher/MainForm.Designer.cs
+++ Windows/ioTank Flasher/ioTank Flasher/MainForm.Designer.cs
@@ -23,16 +23,18 @@ namespace ioTank_Flasher
 		private System.Windows.Forms.Button flash;
 		private System.Windows.Forms.Button help;
 		private System.Windows.Forms.CheckBox Cloud;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.TextBox host;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.TextBox token;
-		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.TextBox endpoint;
-		private System.Windows.Forms.Button status_button;
 		private System.Windows.Forms.Label status;
 		private System.Windows.Forms.Button factory;
-		private System.Windows.Forms.Button button4;
+
+		private System.Windows.Forms.CheckBox Radio;
+		private System.Windows.Forms.Label rssi;
+		private System.Windows.Forms.Label t1;
+		private System.Windows.Forms.Label t2;
+		private System.Windows.Forms.Label h;
+		private System.Windows.Forms.Label uvi;
+		private System.Windows.Forms.Label lux;
 
 
 		
@@ -57,7 +59,7 @@ namespace ioTank_Flasher
 		/// </summary>
 		private void InitializeComponent()
 		{
-			var resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.serial = new System.Windows.Forms.ComboBox();
 			this.password = new System.Windows.Forms.TextBox();
 			this.ssid = new System.Windows.Forms.TextBox();
@@ -67,22 +69,24 @@ namespace ioTank_Flasher
 			this.flash = new System.Windows.Forms.Button();
 			this.help = new System.Windows.Forms.Button();
 			this.Cloud = new System.Windows.Forms.CheckBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.host = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
 			this.token = new System.Windows.Forms.TextBox();
-			this.label6 = new System.Windows.Forms.Label();
-			this.endpoint = new System.Windows.Forms.TextBox();
-			this.status_button = new System.Windows.Forms.Button();
 			this.status = new System.Windows.Forms.Label();
 			this.factory = new System.Windows.Forms.Button();
+			this.Radio = new System.Windows.Forms.CheckBox();
+			this.rssi = new System.Windows.Forms.Label();
+			this.t1 = new System.Windows.Forms.Label();
+			this.t2 = new System.Windows.Forms.Label();
+			this.h = new System.Windows.Forms.Label();
+			this.uvi = new System.Windows.Forms.Label();
+			this.lux = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// serial
 			// 
 			this.serial.DisplayMember = "ports";
 			this.serial.FormattingEnabled = true;
-			this.serial.Location = new System.Drawing.Point(135, 179);
+			this.serial.Location = new System.Drawing.Point(135, 146);
 			this.serial.Name = "serial";
 			this.serial.Size = new System.Drawing.Size(139, 21);
 			this.serial.TabIndex = 5;
@@ -119,7 +123,7 @@ namespace ioTank_Flasher
 			// 
 			// label5
 			// 
-			this.label5.Location = new System.Drawing.Point(14, 182);
+			this.label5.Location = new System.Drawing.Point(14, 149);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(100, 23);
 			this.label5.TabIndex = 9;
@@ -127,9 +131,9 @@ namespace ioTank_Flasher
 			// 
 			// flash
 			// 
-			this.flash.Location = new System.Drawing.Point(101, 234);
+			this.flash.Location = new System.Drawing.Point(14, 198);
 			this.flash.Name = "flash";
-			this.flash.Size = new System.Drawing.Size(173, 29);
+			this.flash.Size = new System.Drawing.Size(260, 64);
 			this.flash.TabIndex = 8;
 			this.flash.Text = "Flash";
 			this.flash.UseVisualStyleBackColor = true;
@@ -137,9 +141,9 @@ namespace ioTank_Flasher
 			// 
 			// help
 			// 
-			this.help.Location = new System.Drawing.Point(101, 269);
+			this.help.Location = new System.Drawing.Point(279, 233);
 			this.help.Name = "help";
-			this.help.Size = new System.Drawing.Size(173, 29);
+			this.help.Size = new System.Drawing.Size(138, 29);
 			this.help.TabIndex = 7;
 			this.help.Text = "Help";
 			this.help.UseVisualStyleBackColor = true;
@@ -148,31 +152,16 @@ namespace ioTank_Flasher
 			// Cloud
 			// 
 			this.Cloud.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-			this.Cloud.Location = new System.Drawing.Point(12, 140);
+			this.Cloud.Location = new System.Drawing.Point(12, 85);
 			this.Cloud.Name = "Cloud";
-			this.Cloud.Size = new System.Drawing.Size(262, 34);
+			this.Cloud.Size = new System.Drawing.Size(261, 34);
 			this.Cloud.TabIndex = 4;
 			this.Cloud.Text = "Send data to the cloud?";
 			this.Cloud.UseVisualStyleBackColor = true;
 			// 
-			// label3
-			// 
-			this.label3.Location = new System.Drawing.Point(12, 65);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(100, 23);
-			this.label3.TabIndex = 15;
-			this.label3.Text = "Host";
-			// 
-			// host
-			// 
-			this.host.Location = new System.Drawing.Point(134, 62);
-			this.host.Name = "host";
-			this.host.Size = new System.Drawing.Size(140, 20);
-			this.host.TabIndex = 2;
-			// 
 			// label4
 			// 
-			this.label4.Location = new System.Drawing.Point(12, 117);
+			this.label4.Location = new System.Drawing.Point(14, 65);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(100, 20);
 			this.label4.TabIndex = 17;
@@ -180,68 +169,110 @@ namespace ioTank_Flasher
 			// 
 			// token
 			// 
-			this.token.Location = new System.Drawing.Point(134, 114);
+			this.token.Location = new System.Drawing.Point(133, 62);
 			this.token.Name = "token";
 			this.token.Size = new System.Drawing.Size(140, 20);
 			this.token.TabIndex = 3;
 			// 
-			// label6
-			// 
-			this.label6.Location = new System.Drawing.Point(12, 91);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(100, 15);
-			this.label6.TabIndex = 19;
-			this.label6.Text = "Endpoint";
-			// 
-			// endpoint
-			// 
-			this.endpoint.Location = new System.Drawing.Point(133, 88);
-			this.endpoint.Name = "endpoint";
-			this.endpoint.Size = new System.Drawing.Size(141, 20);
-			this.endpoint.TabIndex = 18;
-			// 
-			// status_button
-			// 
-			this.status_button.Location = new System.Drawing.Point(12, 234);
-			this.status_button.Name = "status_button";
-			this.status_button.Size = new System.Drawing.Size(83, 29);
-			this.status_button.TabIndex = 21;
-			this.status_button.Text = "Status";
-			this.status_button.UseVisualStyleBackColor = true;
-			this.status_button.Click += new System.EventHandler(this.Button3Click);
-			// 
 			// status
 			// 
 			this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.status.Location = new System.Drawing.Point(14, 205);
+			this.status.Location = new System.Drawing.Point(280, 10);
 			this.status.Name = "status";
-			this.status.Size = new System.Drawing.Size(260, 26);
+			this.status.Size = new System.Drawing.Size(164, 26);
 			this.status.TabIndex = 22;
+			this.status.Text = "IP";
 			// 
 			// factory
 			// 
-			this.factory.Location = new System.Drawing.Point(12, 269);
+			this.factory.Location = new System.Drawing.Point(279, 198);
 			this.factory.Name = "factory";
-			this.factory.Size = new System.Drawing.Size(83, 29);
+			this.factory.Size = new System.Drawing.Size(137, 29);
 			this.factory.TabIndex = 23;
-			this.factory.Text = "Factory Reset";
+			this.factory.Text = "Update Firmware";
 			this.factory.UseVisualStyleBackColor = true;
 			this.factory.Click += new System.EventHandler(this.FactoryClick);
+			// 
+			// Radio
+			// 
+			this.Radio.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.Radio.Location = new System.Drawing.Point(12, 116);
+			this.Radio.Name = "Radio";
+			this.Radio.Size = new System.Drawing.Size(261, 24);
+			this.Radio.TabIndex = 24;
+			this.Radio.Text = "Enable Wifi Radios";
+			this.Radio.UseVisualStyleBackColor = true;
+			// 
+			// rssi
+			// 
+			this.rssi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.rssi.Location = new System.Drawing.Point(280, 36);
+			this.rssi.Name = "rssi";
+			this.rssi.Size = new System.Drawing.Size(164, 26);
+			this.rssi.TabIndex = 25;
+			this.rssi.Text = "RSSI";
+			// 
+			// t1
+			// 
+			this.t1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.t1.Location = new System.Drawing.Point(280, 62);
+			this.t1.Name = "t1";
+			this.t1.Size = new System.Drawing.Size(164, 26);
+			this.t1.TabIndex = 26;
+			this.t1.Text = "T1";
+			// 
+			// t2
+			// 
+			this.t2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.t2.Location = new System.Drawing.Point(280, 85);
+			this.t2.Name = "t2";
+			this.t2.Size = new System.Drawing.Size(164, 26);
+			this.t2.TabIndex = 27;
+			this.t2.Text = "T2";
+			// 
+			// h
+			// 
+			this.h.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.h.Location = new System.Drawing.Point(280, 111);
+			this.h.Name = "h";
+			this.h.Size = new System.Drawing.Size(164, 26);
+			this.h.TabIndex = 28;
+			this.h.Text = "H";
+			// 
+			// uvi
+			// 
+			this.uvi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.uvi.Location = new System.Drawing.Point(280, 137);
+			this.uvi.Name = "uvi";
+			this.uvi.Size = new System.Drawing.Size(164, 26);
+			this.uvi.TabIndex = 29;
+			this.uvi.Text = "UVI";
+			// 
+			// lux
+			// 
+			this.lux.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lux.Location = new System.Drawing.Point(280, 163);
+			this.lux.Name = "lux";
+			this.lux.Size = new System.Drawing.Size(164, 26);
+			this.lux.TabIndex = 30;
+			this.lux.Text = "Lux";
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(287, 308);
+			this.ClientSize = new System.Drawing.Size(430, 276);
+			this.Controls.Add(this.lux);
+			this.Controls.Add(this.uvi);
+			this.Controls.Add(this.h);
+			this.Controls.Add(this.t2);
+			this.Controls.Add(this.t1);
+			this.Controls.Add(this.rssi);
+			this.Controls.Add(this.Radio);
 			this.Controls.Add(this.factory);
 			this.Controls.Add(this.status);
-			this.Controls.Add(this.status_button);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.endpoint);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.token);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.host);
 			this.Controls.Add(this.Cloud);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.help);
@@ -258,6 +289,7 @@ namespace ioTank_Flasher
 			this.MinimizeBox = false;
 			this.Name = "MainForm";
 			this.Text = "ioTank Toolkit";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_Closing);
 			this.Load += new System.EventHandler(this.MainFormLoad);
 			this.ResumeLayout(false);
 			this.PerformLayout();
--- Windows/ioTank Flasher/ioTank Flasher/MainForm.cs
+++ Windows/ioTank Flasher/ioTank Flasher/MainForm.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
-
+using System.Threading;
 
 namespace ioTank_Flasher
 {
@@ -39,15 +39,29 @@ namespace ioTank_Flasher
 			if(count == 2){password.Text = element;}
 			if(count == 3){Cloud.Checked = Boolean.Parse(element);}
 			if(count == 4){token.Text = element;}
-			if(count == 5){host.Text = element;}
-			if(count == 6){endpoint.Text = element;}
+			if(count == 7){Radio.Checked = Boolean.Parse(element);}
+
 
 		}
 	
-   }
+	InitTimer();
+		
+		
 
+
+		
+   }// on create end
+
+    
+      
+
+    
 		void FlashClick(object sender, EventArgs e)
 		{
+	
+			
+
+    
 			string executableLocation = Path.GetDirectoryName(
             Assembly.GetExecutingAssembly().Location);
 			using (var objWriter = new StreamWriter(executableLocation +"\\data\\f"))
@@ -56,8 +70,9 @@ namespace ioTank_Flasher
 		    objWriter.Write(password.Text+Environment.NewLine);
 		    objWriter.Write(Cloud.Checked+Environment.NewLine);
 		    objWriter.Write(token.Text+Environment.NewLine);
-		    objWriter.Write(host.Text+Environment.NewLine);
-			objWriter.Write(endpoint.Text+Environment.NewLine);
+		    objWriter.Write("objectsyndicate.com"+Environment.NewLine);
+			objWriter.Write("/api/v1/post/"+Environment.NewLine);
+			objWriter.Write(Radio.Checked+Environment.NewLine);
 			}
 					
 
@@ -70,11 +85,19 @@ namespace ioTank_Flasher
 			mkspiffs.Start();
 			System.Threading.Thread.Sleep(500);
 
+			var port = new SerialPort(serial.Text, 115200, Parity.None, 8, StopBits.One);
+			
+			
 
+			port.Close();
 			var esptool = new Process();
 			esptool.StartInfo.FileName = strEsptoolCmd;
 			esptool.StartInfo.Arguments = "  -cd nodemcu -cb 115200 -cp "+serial.Text+" -ca 0x300000 -cf out.bin";
+			timer1.Stop();
 			esptool.Start();
+			port.Close();
+			timer1.Start();
+								
 		}
 
 	
@@ -109,9 +132,98 @@ namespace ioTank_Flasher
 		
 		   return false;
 		}	
+	
+	
+		void TokenTooltip(object sender, EventArgs e){
+         // Create the ToolTip and associate with the Form container.
+         ToolTip toolTip1 = new ToolTip();
+
+         // Set up the delays for the ToolTip.
+         toolTip1.AutoPopDelay = 0;
+         toolTip1.InitialDelay = 0;
+         toolTip1.ReshowDelay = 500;
+         // Force the ToolTip text to be displayed whether or not the form is active.
+         toolTip1.ShowAlways = true;
+
+         // Set up the ToolTip text for the Button and Checkbox.
+         toolTip1.SetToolTip(token, "Tokens are issued at objectsyndicate.com/devices");
+         //toolTip1.SetToolTip(this.checkBox1, "My checkBox1");
+			
+		}
+
+
+
+		void SSIDTooltip(object sender, EventArgs e){
+         // Create the ToolTip and associate with the Form container.
+         ToolTip toolTip1 = new ToolTip();
+
+         // Set up the delays for the ToolTip.
+         toolTip1.AutoPopDelay = 0;
+         toolTip1.InitialDelay = 0;
+         toolTip1.ReshowDelay = 500;
+         // Force the ToolTip text to be displayed whether or not the form is active.
+         toolTip1.ShowAlways = true;
+
+         // Set up the ToolTip text for the Button and Checkbox.
+         toolTip1.SetToolTip(ssid, "Device SSID is case sensitve");
+         //toolTip1.SetToolTip(this.checkBox1, "My checkBox1");
+			
+		}
+		
+		
+		void PwdTooltip(object sender, EventArgs e){
+         // Create the ToolTip and associate with the Form container.
+         ToolTip toolTip1 = new ToolTip();
+
+         // Set up the delays for the ToolTip.
+         toolTip1.AutoPopDelay = 0;
+         toolTip1.InitialDelay = 0;
+         toolTip1.ReshowDelay = 500;
+         // Force the ToolTip text to be displayed whether or not the form is active.
+         toolTip1.ShowAlways = true;
+
+         // Set up the ToolTip text for the Button and Checkbox.
+         toolTip1.SetToolTip(password, "WPA security required");
+         //toolTip1.SetToolTip(this.checkBox1, "My checkBox1");
+			
+		}
+	
+		
+		
+		 void RadioTooltip(object sender, EventArgs e){
+         // Create the ToolTip and associate with the Form container.
+         ToolTip toolTip1 = new ToolTip();
+
+         // Set up the delays for the ToolTip.
+         toolTip1.AutoPopDelay = 0;
+         toolTip1.InitialDelay = 0;
+         toolTip1.ReshowDelay = 500;
+         // Force the ToolTip text to be displayed whether or not the form is active.
+         toolTip1.ShowAlways = true;
+
+         // Set up the ToolTip text for the Button and Checkbox.
+         toolTip1.SetToolTip(Radio, "Enable Wifi Radio (USB only if disabled)");
+         //toolTip1.SetToolTip(this.checkBox1, "My checkBox1");
+		}
+		
+		 void CloudTooltip(object sender, EventArgs e){
+         // Create the ToolTip and associate with the Form container.
+         ToolTip toolTip1 = new ToolTip();
+
+         // Set up the delays for the ToolTip.
+         toolTip1.AutoPopDelay = 0;
+         toolTip1.InitialDelay = 0;
+         toolTip1.ReshowDelay = 500;
+         // Force the ToolTip text to be displayed whether or not the form is active.
+         toolTip1.ShowAlways = true;
+
+         // Set up the ToolTip text for the Button and Checkbox.
+         toolTip1.SetToolTip(Cloud, "Send data to the cloud");
+         //toolTip1.SetToolTip(this.checkBox1, "My checkBox1");
+		
+		}
 
 		
-	
 		void MainFormLoad(object sender, EventArgs e)
 		{
 	
@@ -121,10 +233,41 @@ namespace ioTank_Flasher
 			Process.Start("https://www.objectsyndicate.com/docs/");
 
 		}
-		void Button3Click(object sender, EventArgs e)
-		{
-		var port = new SerialPort(serial.Text, 115200, Parity.None, 8, StopBits.One);
-		port.ReadTimeout = 2000;
+		
+		
+			
+
+		
+		
+		
+	
+System.Windows.Forms.Timer timer1; 
+public void InitTimer()
+{
+    timer1 = new System.Windows.Forms.Timer();
+    timer1.Tick += new EventHandler(timer1_Tick);
+    timer1.Interval = 2000; // in miliseconds
+    timer1.Start();
+}
+
+
+
+Thread thread;
+void timer1_Tick(object sender, EventArgs e){
+	thread = new Thread(new ThreadStart(Thread1));
+	
+	thread.Start();
+
+}
+	
+void Thread1(){
+
+	string val = "";
+	serial.Invoke(new Action(() => {  val=serial.Text; }));
+	try{
+		var port = new SerialPort(val, 115200, Parity.None, 8, StopBits.One);
+	
+		port.ReadTimeout = 500;
   		try 
         {	
 		port.Open();
@@ -134,7 +277,7 @@ namespace ioTank_Flasher
 
 		if (IsIP(data))
 		{
-			status.Text = data;
+			status.Invoke(new Action(() => { status.Text = data; }));
 		}
 		else{
 			port.Open();
@@ -143,7 +286,7 @@ namespace ioTank_Flasher
 
 			if (IsIP(data))
 			{
-				status.Text = data;
+				status.Invoke(new Action(() => { status.Text = data; }));
 			}
 			else{
 				port.Open();
@@ -152,11 +295,49 @@ namespace ioTank_Flasher
 
 				if (IsIP(data))
 				{
-					status.Text = data;
+					status.Invoke(new Action(() => { status.Text = data; }));
 				}
 				else{
 
-		status.Text = data;
+
+		try{
+		var dataList = data.Split('_');
+
+		var IP = dataList[0];
+		var RSSI = dataList[1];
+		var T1 = dataList[2];
+		var T2 = dataList[3];
+		var H = dataList[4];
+		var UV = dataList[5];
+		var L = dataList[6];		
+		
+		
+		
+		//progressBar1.Invoke(new Action(() => { progressBar1.Value = Convert.ToInt32(Convert.ToDouble(T1)); }));
+		
+		status.Invoke(new Action(() => { status.Text ="IP Address: "+IP; }));
+		rssi.Invoke(new Action(() => { rssi.Text ="RSSI: "+RSSI; }));
+		t1.Invoke(new Action(() => { t1.Text ="Temp 1: "+T1+"C"; }));
+		t2.Invoke(new Action(() => { t2.Text ="Temp 2: "+T2+"C"; }));
+		h.Invoke(new Action(() => { h.Text ="Humidity: "+H+"%"; }));
+		uvi.Invoke(new Action(() => { uvi.Text ="UV Index: "+UV; }));
+		lux.Invoke(new Action(() => { lux.Text ="Lux: "+L; }));
+		
+		} catch{
+		status.Invoke(new Action(() => { status.Text = data; }));
+		rssi.Invoke(new Action(() => { rssi.Text ="RSSI: "; }));
+		t1.Invoke(new Action(() => { t1.Text ="Temp 1: "; }));
+		t2.Invoke(new Action(() => { t2.Text ="Temp 2: "; }));
+		h.Invoke(new Action(() => { h.Text ="Humidity: "; }));
+		uvi.Invoke(new Action(() => { uvi.Text ="UV Index: "; }));
+		lux.Invoke(new Action(() => { lux.Text ="Lux: "; }));
+		
+		}
+					
+					
+					
+					
+					
 				}
 				
 			}
@@ -165,21 +346,58 @@ namespace ioTank_Flasher
 
 		
 		
-		port.Close();
+
 		}
 
   		catch (TimeoutException ex)
         {
-        status.Text = "Communication error";
+        status.Invoke(new Action(() => { status.Text = "Communication error"; }));
+		rssi.Invoke(new Action(() => { rssi.Text ="RSSI: "; }));
+		t1.Invoke(new Action(() => { t1.Text ="Temp 1: "; }));
+		t2.Invoke(new Action(() => { t2.Text ="Temp 2: "; }));
+		h.Invoke(new Action(() => { h.Text ="Humidity: "; }));
+		uvi.Invoke(new Action(() => { uvi.Text ="UV Index: "; }));
+		lux.Invoke(new Action(() => { lux.Text ="Lux: "; }));
+		
         }
         catch (UnauthorizedAccessException ex)
         {
-        status.Text = "Communication error";
+        status.Invoke(new Action(() => { status.Text = "Communication error"; }));
+        rssi.Invoke(new Action(() => { rssi.Text ="RSSI: "; }));
+		t1.Invoke(new Action(() => { t1.Text ="Temp 1: "; }));
+		t2.Invoke(new Action(() => { t2.Text ="Temp 2: "; }));
+		h.Invoke(new Action(() => { h.Text ="Humidity: "; }));
+		uvi.Invoke(new Action(() => { uvi.Text ="UV Index: "; }));
+		lux.Invoke(new Action(() => { lux.Text ="Lux: "; }));
+		
         }
-    
-  }
+    	
+			port.Close();
+
+	}catch{
+		if (!ending){
+		ending = true;
+		MessageBox.Show("No Serial port detected install the included drivers.");
+		Environment.Exit(Environment.ExitCode);
+		} 
+	}		
+	} // close
+		
+bool ending;
+		
+	void MainForm_Closing(object sender, FormClosingEventArgs e){
+	ending = true;	
+	try{
+	thread.Abort();
+	}catch{
+	}
+
+}
+	
+		
 		void FactoryClick(object sender, EventArgs e)
 		{
+timer1.Stop();
 			string executableLocation = Path.GetDirectoryName(
             Assembly.GetExecutingAssembly().Location);
 			
@@ -201,7 +419,7 @@ namespace ioTank_Flasher
 			spiff.StartInfo.Arguments = " -cd nodemcu -cb 115200 -cp "+serial.Text+" -ca 0x300000 -cf ioTank.spiffs.bin";
 			spiff.Start();
 			
-
+timer1.Start();
 			port.Close();
 		}
 	}
